398b45345954ee0dfa43d8c068126330f534f753
Eduardo Corrêa <tavaresdu@gmail.com>
2019-04-02 16:43:37 -0300

    Curso 2 aula 2

--- src/main/java/br/edu/tavaresdu/springmvc/loja/configuration/AppWebConfiguration.java
+++ src/main/java/br/edu/tavaresdu/springmvc/loja/configuration/AppWebConfiguration.java
@@ -17,14 +17,19 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @EnableWebMvc
@@ -75,6 +80,17 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
         GuavaCacheManager manager = new GuavaCacheManager();
         manager.setCacheBuilder(builder);
         return manager;
+    }
+
+    @Bean
+    public ViewResolver contentNegociationViewResolver(ContentNegotiationManager manager) {
+        List<ViewResolver> viewResolverList = new ArrayList<>();
+        viewResolverList.add(internalResourceViewResolver());
+        viewResolverList.add(new JsonViewResolver());
+        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+        resolver.setViewResolvers(viewResolverList);
+        resolver.setContentNegotiationManager(manager);
+        return resolver;
     }
 
     @Override
---
+++ src/main/java/br/edu/tavaresdu/springmvc/loja/configuration/JsonViewResolver.java
@@ -0,0 +1,17 @@
+package br.edu.tavaresdu.springmvc.loja.configuration;
+
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import java.util.Locale;
+
+public class JsonViewResolver implements ViewResolver {
+
+    @Override
+    public View resolveViewName(String s, Locale locale) throws Exception {
+        MappingJackson2JsonView jsonView = new MappingJackson2JsonView();
+        jsonView.setPrettyPrint(true);
+        return jsonView;
+    }
+}
